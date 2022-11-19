d86e72b96cd7e837f1fc8b4550e6a3945427b643
Lola <altnazlo@mail.ru>
2022-11-19 14:47:35 +0700

    восьмая_задача

---
+++ восьмая_задача/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+Console.WriteLine();
+int n = int.Parse(str);
+    
+for (int i = 1; i <= n; i++)
+    if (i % 2 == 0)
+    {
+        Console.WriteLine("чётные числа: " + i);
+    }
