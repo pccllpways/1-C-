6de9d10ebdd9c562b1dc1fbb956a9e9ee4c44537
Lola <altnazlo@mail.ru>
2022-11-19 14:41:54 +0700

    шестая_задача

---
+++ шестая_задача/Program.cs
@@ -0,0 +1,15 @@
+﻿// a = 4
+// a = -3
+// a = 7
+
+Console.WriteLine("Напишите число: ");
+string str = Console.ReadLine();
+int a = int.Parse(str);
+if((a % 2) == 0)
+{
+    Console.WriteLine("чётное");
+}
+else
+{
+    Console.WriteLine("нечётное");
+}
