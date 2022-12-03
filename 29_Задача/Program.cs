63ba05bed9c3a67a842c313fe4e558f4c1714ef0
Lola <maximizer38@gmail.com>
2022-12-03 23:47:21 +0700

    29_Задача

---
+++ 29_Задача/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 29: Напишите программу, которая генерирует массив из 8 
+// элементов - случайных чисел (от 0 до 20) и выводит их на экран.
+
+int[] array = new int[20];
+for (int i = 0; i < 8; i++)
+{
+    int random = new Random().Next(0, 21);
+    array[i] = random;
+    Console.WriteLine(array[i]);
+}
