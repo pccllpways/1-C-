043ce4f294274a21dbbe5833bd25bda847cc056d
Lola <maximizer38@gmail.com>
2022-12-03 23:45:59 +0700

    27_Задача

---
+++ 27_Задача/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт 
+// сумму цифр в числе.
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int przv = 0;
+while (num > 0)
+{
+    przv = przv + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(przv);
