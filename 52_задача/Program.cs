2e5068c5a15e8b0f7ddcb97e7d56e28f30a4b5d8
Lola <maximizer38@gmail.com>
2022-12-20 15:08:02 +0700

    52_задача

---
+++ 52_задача/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+// арифметическое элементов в каждом столбце.
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max + 1);
+        }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+void Average(int[,] array)
+{
+    double[] result = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum = sum + array[i, j];
+        }
+        result[j] = sum / array.GetLength(0);
+    }
+    Console.WriteLine($"|{String.Join("|", result)}|");
+}
+Console.Write("Введите длину строк: ");
+int line = int.Parse(Console.ReadLine());
+Console.Write("Введите длину столбцов: ");
+int column = int.Parse(Console.ReadLine());
+int[,] array = GetArray(line, column, 0, 10);
+PrintArray(array);
+Average(array);
